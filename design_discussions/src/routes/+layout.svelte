52c8be97924eb05165dbf355f58d16544dc9eaeb
sixLUKEY <marcoslukerojo@gmail.com>
2024-01-17 16:00:49 +0200

    january and february pages created

---
+++ design_discussions/src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script>
+    import "../app.css";
+
+    let routes = [
+    {
+        name: 'home',
+        href: '/'
+    },
+    {
+        name: 'January',
+        href: 'january'
+    },
+    {
+        name: 'February',
+        href: 'february'
+    }
+    ]
+</script>
+
+<main>
+    <nav>
+        <ul>
+            {#each routes as { name, href }}
+                <li class=" list-none text-2xl">
+                    <a {href}>{name}</a>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+
+    <slot />
+</main>
---
+++ design_discussions/src/routes/january/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    let weeks = [
+        {
+            name: '1',
+            href: '1'
+        },
+        {
+            name: '2',
+            href: '2'
+        }
+    ]
+</script>
+
+<h1>
+    DESIGN IN JANUARY?!
+</h1>
+
+<nav>
+    <ul>
+        {#each weeks as { name, href }}
+        <li>
+            <a href={`/january/${href}`}>Week {name}</a>
+        </li>
+        {/each}
+    </ul>
+</nav>
+
+<slot/>
